6129a28a5e4422f15cc6e2c7f47fe65f804ee5d4
Justus Witmer <justus.witmer@gmail.com>
2023-06-13 21:33:21 -0400

    changing post data

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,9 +2,9 @@ import Post from "../components/Post"
 import Header from "../components/Header"
 import { useLoaderData } from "@remix-run/react"
 import { gql } from "@apollo/client"
-import { client }  from "../lib/apollo"
+import { client } from "../lib/apollo"
 
-export async function loader (){
+export async function loader() {
   const PostsQuery = gql`
         query GetPosts {
             posts {
@@ -17,27 +17,28 @@ export async function loader (){
             }
         }
     `
-    const response = await client.query({
-        query: PostsQuery
-    })
+  const response = await client.query({
+    query: PostsQuery
+  })
 
-    const posts = response?.data?.posts?.nodes
-    return posts
+  const posts = response?.data?.posts?.nodes
+  return posts
 }
 
 
 export default function Index() {
-  const posts = useLoaderData();
+  const posts = loader();
+  // const posts = useLoaderData();
   return (
     <div>
       <Header title="Home Page" ></Header>
-        <div className="grid gap-8 grid-cols-1 lg:grid-cols-3 p-6">
-          {posts.map(post => {
-              return (
-                <Post post={post} key={post.title}></Post>
-              )
-            })}
-        </div>
+      <div className="grid gap-8 grid-cols-1 lg:grid-cols-3 p-6">
+        {posts.map(post => {
+          return (
+            <Post post={post} key={post.title}></Post>
+          )
+        })}
+      </div>
     </div>
   );
 }
